af1ed886cac203a54be49537d9b49bc8759c40a8
Alex Ray <alex@alex-laptop.(none)>
2009-11-15 00:26:00 -0500

    major changes, completely broke. now has an irc package

--- irc.go
+++ irc.go
@@ -1,11 +1,11 @@
 // IRC - Internet Relay Chat package provides a convenient method of writing IRC clients and chat bots (RFC 2812).
-package irc 
+package irc
 
 import (
     "net";
+    "os";
+
     "strings";
-    "log"; // TODO: implement actual Loggers
-    "bufio";
     "strconv";
 )
 
@@ -14,23 +14,23 @@ import (
 type UserMode int
 
 const (
-    FlagWallops = 1 << 2; // Get wallops (globally broadcast messages)
+    FlagWallops   = 1 << 2; // Get wallops (globally broadcast messages)
     FlagInvisible = 1 << 3; // User doesn't show up in WHO listings
 )
 
 // IRCConn is an implementation of the net.Conn interface for IRC network connections.
-type IRCConn struct{
+type IRCConn struct {
     Nickname string;
     Usermode int;
-    tcpConn *net.TCPConn; 
-    }
+    tcpConn  *net.TCPConn;
+}
 
 // DialIRC() is like net.Dial() but can only connect to IRC networks
 // and returns a IRCConn structure.
-func IRCDial(net, laddr, raddr string) (c IRCConn, err os.Error) {
-    var conn *TCPConn;
-    if conn, err := net.Dial(net, laddr, raddr); err != nil {
-        return nil, err;
+func IRCDial(netc, laddr, raddr string) (IRCConn, os.Error) {
+    conn, err := net.Dial(netc, laddr, raddr);
+    if err != nil {
+        return nil, err
     }
     var c *IRCConn;
     c.tcpConn = conn;
@@ -43,11 +43,11 @@ func (c *IRCConn) Login(nickname string, usermode int) os.Error {
     // Form NICK message
     loginMessage := "NICK " + nickname + "\r\n";
     // Form USER message
-    username := "gobot";        // TODO: make this a parameter
+    username := "gobot";                   // TODO: make this a parameter
     realname := "Go Programming Language"; // TODO: make this a paramater
     loginMessage += "USER " + username + " " + strconv.Itoa(usermode) + " * :" + realname + "\r\n";
     // Write to connection and return result
-    _, err := c.Write(strings.Bytes(loginMessage);
+    _, err := c.Write(strings.Bytes(loginMessage));
     return err;
 }
 
@@ -55,21 +55,21 @@ func (c *IRCConn) Login(nickname string, usermode int) os.Error {
 func (c *IRCConn) PrivMsg(recipient, message string) os.Error {
     privMessage := "PRIVMSG " + recipient + " :" + message + "\r\n";
     // Write to connection and return result
-    _, err := c.Write(strings.Bytes(loginMessage);
+    _, err := c.Write(strings.Bytes(loginMessage));
     return err;
 }
 
 // IRCChan - IRC Channel
-type IRCChan struct{
+type IRCChan struct {
     Conn IRCConn; // IRC Connection this channel is on
     Chan string;  // IRC Channel name
 }
 
 // Join() - Join an IRC Channel on an IRC Connection
-func (c *IRCConn) Join(channame string) *IRCChan, os.Error {
+func (c *IRCConn) Join(channame string) (*IRCChan, os.Error) {
     joinMessage := "JOIN " + channame + "\r\n";
     if _, err := c.tcpConn.Write(strings.Bytes(loginMessage)); err != nil {
-        return nil, err;
+        return nil, err
     }
     var ircChan *IRCChan;
     ircChan.Chan = channame;
