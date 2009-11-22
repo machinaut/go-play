fba6088df382a13d86449da42f1be3803d31dc0f
Alex Ray <alex@alex-laptop.(none)>
2009-11-21 20:55:47 -0500

    peggy

--- peg.go
+++ peg.go
@@ -1,3 +1,13 @@
 # Hierarchical syntax
 Grammar     <- Spacing Definition+ EndOfFile
+Definition  <- Identifier LEFTARROW Expression
+
+Expression  <- Sequence (SLASH Sequence)*
+Sequence    <- Prefix*
+Prefix      <- (AND / NOT)? Suffix
+Suffix      <- Primary (QUESTION / STAR / PLUS)?
+Primary     <- Identifier !LEFTARROW
+             / OPEN Expression CLOSE
+             / Literal / Class / DOT
+
 
